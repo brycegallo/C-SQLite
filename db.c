ea447507438fa587ecb8041bd6096a0cf9ab41a1
brycegallo <76966016+brycegallo@users.noreply.github.com>
2025-04-14 15:06:37 -0600

    Write skeleton code for SQL Virtual machine set up program to take and respond to specific commands

--- db.c
+++ db.c
@@ -12,6 +12,18 @@ typedef struct {
     ssize_t input_length;
 } InputBuffer;
 
+typedef enum { META_COMMAND_SUCCESS, META_COMMAND_UNRECOGNIZED_COMMAND } MetaCommandResult;
+
+typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
+
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+
+typedef struct {
+    StatementType type;
+} Statement;
+
+/*** Functions ***/
+
 // This function creates a new InputBuffer*
 InputBuffer* new_input_buffer() {
     InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
@@ -20,6 +32,15 @@ InputBuffer* new_input_buffer() {
     input_buffer->input_length = 0;
 
     return input_buffer;
+}
+
+// This is just a wrapper for existing functionality in main, but leaves room for more commands
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+	exit(EXIT_SUCCESS);
+    } else {
+	return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
 }
 
 // This prints a prompt to the user, we'll call this before reading each line of input
@@ -55,6 +76,31 @@ void close_input_buffer(InputBuffer* input_buffer) {
     free(input_buffer);
 }
 
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+    // We use strncmp() for "insert" because that keyword will be followed by data
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+	statement->type = STATEMENT_INSERT;
+	return PREPARE_SUCCESS;
+    }
+    if (strcmp(input_buffer->buffer, "select") == 0) {
+	statement->type = STATEMENT_SELECT;
+	return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement* statement) {
+    switch (statement->type) {
+	case (STATEMENT_INSERT):
+	    printf("This is where we would do an insert.\n");
+	    break;
+	case (STATEMENT_SELECT):
+	    printf("This is where we would do a select.\n");
+	    break;
+    }
+}
+
 int main (int argc, char* argv[]) {
     (void)argc; (void) argv; // prevent compiler warnings
     InputBuffer* input_buffer = new_input_buffer();
@@ -63,16 +109,28 @@ int main (int argc, char* argv[]) {
 	print_prompt();
 	read_input(input_buffer);
 	
+	// Here we check for non-SQL statements (called "meta-commands") which all start with a . and prepare to handle them in a separate function
 	if (input_buffer->buffer[0] == '.') {
 	    switch (do_meta_command(input_buffer)) {
 		case (META_COMMAND_SUCCESS):
 		    continue;
 		case (META_COMMAND_UNRECOGNIZED_COMMAND):
-		    printf("Unrecognized command '%'\n", input_buffer->buffer);
+		    printf("Unrecognized command '%s' \n", input_buffer->buffer);
 		    continue;
 	    }
 	}
+	// Here we convert the line of input into our internal representation of a statement
 	Statement statement;
-	switch
+	switch (prepare_statement(input_buffer, &statement)) {
+	    case (PREPARE_SUCCESS):
+		break;
+	    case (PREPARE_UNRECOGNIZED_STATEMENT):
+		printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+		continue;
+	}
+
+	// We pass the statement output of the above conversion into a function that will act as our virtual machine
+	execute_statement(&statement);
+	printf("Executed.\n");
     }
 }
