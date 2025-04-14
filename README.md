38e06dd88af67fa86f2a7927c151d3e23ceba320
brycegallo <76966016+brycegallo@users.noreply.github.com>
2025-04-13 19:49:24 -0600

    Update README.md, start working on SQL Compiler included SQL Architecture Diagram in readme for my own reference, modify main()

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # C-SQLite
 A clone of SQLite written in C
 Written by following https://cstack.github.io/db_tutorial/
+
+Basic Representation of Architecture:
+
+[Core]
+Interface
+↓
+SQL Command Processor
+↓
+    [SQL Compiler] Tokenizer -> Parser -> Code Generator 
+↓
+Virtual Machine
+↓
+[Backend]
+B-Tree
+↓
+Pager
+↓
+OS Interface
--- db.c
+++ db.c
@@ -62,12 +62,17 @@ int main (int argc, char* argv[]) {
     while (true) {
 	print_prompt();
 	read_input(input_buffer);
-
-	if (strcmp(input_buffer->buffer, ".exit") == 0) {
-	    close_input_buffer(input_buffer);
-	    exit(EXIT_SUCCESS);
-	} else {
-	    printf("Unrecognized command '%s' .\n", input_buffer->buffer);
+	
+	if (input_buffer->buffer[0] == '.') {
+	    switch (do_meta_command(input_buffer)) {
+		case (META_COMMAND_SUCCESS):
+		    continue;
+		case (META_COMMAND_UNRECOGNIZED_COMMAND):
+		    printf("Unrecognized command '%'\n", input_buffer->buffer);
+		    continue;
+	    }
 	}
+	Statement statement;
+	switch
     }
 }
